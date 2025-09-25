1e585aabf47fb1147bf21d5b66db8aff2e9f361e
Wellcoutinhoch <wellington.coutinho@escolar.ifrn.edu.br>
2025-09-25 17:21:45 +0000

    aula2

--- projsala/imc/urls.py
+++ projsala/imc/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.index,name='index'),
-    
+    path('nome/',views.nome, name='nome'),
+    path('tabuada/',views.tabuada, name='tabuada'),
+    path('cinco/',views.cinco, name='tabuada'),
 ]
--- projsala/imc/views.py
+++ projsala/imc/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-
 # Create your views here.
- def index(request):
-    return HttpResponse("<h1 Bem vindo(a) à aplicação IMC</h1>")
+def index(request):
+    return HttpResponse("<h1>Bem vindo(a) à aplicação IMC</h1>")
 
 def nome(request):
-    return HttpResponse("<h1 Wellington Coutinho </h1>")
+    return HttpResponse("<h1>Wellington Coutinho</h1>")
+
+def tabuada(request):
+    html = '<table border="1"><tr><td><b>Tabuada de Sete</b></td></tr>'
+    for i in range(1, 11):
+        html += f"<tr><td>7 x {i} = {7 * i}</td></tr>"
+    html += "</table>"
+
+    return HttpResponse(html)
