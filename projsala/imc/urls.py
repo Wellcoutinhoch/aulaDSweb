f0a80dc5d650e6b8ab3dea469bb2a017399a4f36
Wellcoutinhoch <wellington.coutinho@escolar.ifrn.edu.br>
2025-10-01 20:57:45 +0000

    aula6

--- projsala/imc/urls.py
+++ projsala/imc/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
 from . import views
-urlpatterns=[
-    path('',views.index,name='index'),
-    path('nome/',views.nome, name='nome'),
-    path('tabuada/',views.tabuada, name='tabuada'),
-    path('cinco/',views.cinco, name='tabuada'),
+from django.urls import path
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("calcular/", views.calcular_imc, name="calcular_imc" ),
 ]
--- projsala/imc/views.py
+++ projsala/imc/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from .models import Question
 def index(request):
-    return HttpResponse("<h1>Bem vindo(a) à aplicação IMC</h1>")
+  return render(request, 'index.html')
+def calcular_imc(request):
+    altura = float(request.GET.get('altura'))
+    peso = float(request.GET.get('peso'))
+    imc= peso/(altura*altura)
+    if imc < 18.5:
+        classificacao = 'Abaixo do peso'
+    elif imc < 24.9:
+        classificacao = 'Peso normal'
+    elif imc < 29.9:
+        classificacao = 'Sobrepeso'
+    else:
+        classificacao = 'Obesidade'
+    contexto= {
+        'imc': imc,
+        'classificacao': classificacao,
+        'altura': altura,
+        'peso': peso,
+    }
+    return render(request, 'resultado_imc.html', contexto)
 
-def nome(request):
-    return HttpResponse("<h1>Wellington Coutinho</h1>")
-
-def tabuada(request):
-    html = '<table border="1"><tr><td><b>Tabuada de Sete</b></td></tr>'
-    for i in range(1, 11):
-        html += f"<tr><td>7 x {i} = {7 * i}</td></tr>"
-    html += "</table>"
-
-    return HttpResponse(html)
